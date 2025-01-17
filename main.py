6b3c8c7e3cb7007513e07d425b451092e2a2a9f0
Danil <papapapiroska@gmail.com>
2025-01-18 02:12:00 +0300

    rabbitmq

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import redis
 import sentry_sdk
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
 
 from celery_config import get_book
 from models import Author, Book, BookOwner, Category, SessionLocal, User
@@ -22,7 +23,6 @@ from schemas import (
     UserCreate,
     UserResponse,
 )
-
 SENTRY_DSN = os.getenv("SENTRY_DSN")
 redis_host = os.getenv("REDIS_HOST")
 redis_port = int(os.getenv("REDIS_PORT"))
@@ -45,6 +45,7 @@ sentry_sdk.init(
 app = FastAPI()
 
 
+
 # Получение сессии базы данных
 def get_db():
     db = SessionLocal()
@@ -55,40 +56,37 @@ def get_db():
 
 @app.get("/books/{book_name}")
 def get_book_by_name(book_name: str):
-    random_int = random.randint(1, 100000)
-    task_number = str(random_int)
-    cache_key = f'{book_name}'
-    cached_data = redis_client.get(cache_key)
-    if cached_data:
-        print('данные взяты из кэша')
-        decoded_string = cached_data.decode('utf-8')
-        data_dict = json.loads(decoded_string)
-        print(data_dict)
-        redis_client.setex(task_number, 60, decoded_string)
-        return {"task_number": task_number, "status": "ready by cache"}
+    # cache_key = f'{book_name}'
+    # cached_data = redis_client.get(cache_key)
+    # if cached_data:
+    #     print('данные взяты из кэша')
+    #     decoded_string = cached_data.decode('utf-8')
+    #     data_dict = json.loads(decoded_string)
+    #     print(data_dict)
+    #     redis_client.setex(task_number, 60, decoded_string)
+    #     return {"task_number": task_number, "status": "ready by cache"}
     print('запускаем запрос celery')
 
-    task_number = str(random_int)
-    get_book.delay(book_name, task_number)
-    return {"task_number": task_number, "status": "started"}
 
-@app.get("/tasks/{task_number}")
-def get_result(task_number: str):
-    result_red = redis_client.get(task_number)
-    if result_red:
-        result = result_red.decode('utf-8')
-        data_dict = json.loads(result)
-        name = data_dict['name']
-        data_json = json.dumps(data_dict)
-        redis_client.setex(name, 60, data_json)
-        return json.loads(result)
+    task = get_book.delay(book_name)
+    return {"task_number": task.id, "status": "started"}
+
+@app.get("/tasks/{task_id}")
+def get_result(task_id: str):
+    task = get_book.AsyncResult(task_id)
+    if task:
+        a = task.get()
+        result_str = a.replace("'", '"')
+
+        result_dict = json.loads(result_str)
+        result = {"status": "SUCCESS", "result": result_dict}
+        return JSONResponse(result)
     else:
         return('результата пока нет, попробуйте позже')
 
 # Добавление нового пользователя
 @app.post("/users/", response_model=dict)
-def create_user(user: UserCreate):
-    db: Session = Depends(get_db)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.email == user.email).first()
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -98,8 +96,7 @@ def create_user(user: UserCreate):
     db.refresh(new_user)
     return {"id": new_user.id, "name": new_user.name, "email": new_user.email}
 @app.post("/takebook/", response_model=dict)
-def take_book(act: TakeBook):
-    db: Session = Depends(get_db)
+def take_book(act: TakeBook, db: Session = Depends(get_db)):
     new_record = BookOwner(user_id=act.user_id, book_id=act.book_id)
     db.add(new_record)
     db.commit()
@@ -109,8 +106,7 @@ def take_book(act: TakeBook):
 
 # Удаление пользователя по ID
 @app.delete("/users/{user_id}")
-def delete_user(user_id: int):
-    db: Session = Depends(get_db)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -121,25 +117,21 @@ def delete_user(user_id: int):
 
 # Получение списка всех пользователей
 @app.get("/users/", response_model=List[UserResponse])
-def read_users():
-    db: Session = Depends(get_db)
+def read_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
 @app.get("/book_owners/", response_model=List[BookOwnerResponse])
-def read_bookowners():
-    db: Session = Depends(get_db)
+def read_bookowners(db: Session = Depends(get_db)):
     bookowners = db.query(BookOwner).all()
     return bookowners
 
 @app.get("/books/", response_model=List[BookResponse])
-def read_books():
-    db: Session = Depends(get_db)
+def read_books(db: Session = Depends(get_db)):
     books = db.query(Book).all()
     return books
 
 @app.post("/books/", response_model=dict)
-def add_book(book: AddBook):
-    db: Session = Depends(get_db)
+def add_book(book: AddBook, db: Session = Depends(get_db)):
     new_book = Book(name=book.name, author_id=book.author_id, category_id=book.category_id)
     db.add(new_book)
     db.commit()
@@ -147,14 +139,13 @@ def add_book(book: AddBook):
     return {"id": new_book.id, "name": new_book.name, "author": new_book.author_id, "category": new_book.category_id}
 
 @app.get("/authors/", response_model=List[AuthorResponse])
-def read_authors():
+def read_authors(db: Session = Depends(get_db)):
     db: Session = Depends(get_db)
     authors = db.query(Author).all()
     return authors
 
 @app.post("/authors/", response_model=dict)
-def add_author(author: AddAuthor):
-    db: Session = Depends(get_db)
+def add_author(author: AddAuthor, db: Session = Depends(get_db)):
     new_author = Author(name=author.name, second_name=author.second_name)
     db.add(new_author)
     db.commit()
@@ -162,18 +153,14 @@ def add_author(author: AddAuthor):
     return {"id": new_author.id, "name": new_author.name, "second_name": new_author.second_name}
 
 @app.get("/categories/", response_model=List[CategoryResponse])
-def read_categories():
-    db: Session = Depends(get_db)
+def read_categories(db: Session = Depends(get_db)):
     categories = db.query(Category).all()
     return categories
 
 @app.post("/categories/", response_model=dict)
-def add_category(category: AddCategory):
-    db: Session = Depends(get_db)
+def add_category(category: AddCategory, db: Session = Depends(get_db)):
     new_category = Category(name=category.name)
     db.add(new_category)
     db.commit()
     db.refresh(new_category)
     return {"id": new_category.id, "name": new_category.name}
-
-
